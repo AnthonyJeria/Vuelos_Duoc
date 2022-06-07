08ac421550ded4a123732d5dea0b7233709e2e51
AnthonyJeria <anth.jeria@duocuc.cl>
2022-06-07 17:13:29 -0400

    Se crea el menu

---
+++ Funciones_vuelos.py
@@ -0,0 +1,11 @@
+def printMenu():
+  print("-"*40)
+  print("Vuelos Duoc")
+  print("-"*40)
+  print("1.   Ver asientos disponibles")
+  print("2.   Comprar asiento")
+  print("3.   Anular vuelo")
+  print("4.   Modificar datos de pasajero")
+  print("5.   Salir")
+  print("-"*40)
+  print("Ingrese el numero de la función que desea utilizar")
